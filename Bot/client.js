b7f37a310d92ec553640ebcd5cc40dab1d4499b2
Anuj <anujtiwari4454@outlook.com>
2025-04-10 21:50:32 +0530

    change

--- Bot/client.js
+++ Bot/client.js
@@ -4,7 +4,7 @@ const config = require("./config/config");
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: {
+   puppeteer: {
     headless: true,
     args: [
       "--no-sandbox",
